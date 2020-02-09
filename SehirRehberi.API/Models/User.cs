75d64acbadbd6975a1daf7d1fc68faa008d45fd4
Ertuğrul Güler <ertglr.61@gmail.com>
2020-02-09 22:45:37 +0300

    Db modelleri oluşturuldu.

---
+++ SehirRehberi.API/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+        public List<City> Cities { get; set; } = new List<City>();
+    }
+}
