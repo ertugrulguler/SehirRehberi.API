72aca3089598753baf2148e13826e810f81d69a3
Ertuğrul Güler <ertglr.61@gmail.com>
2020-02-10 10:34:51 +0300

    AutoMapper projeye dahil edildi.

---
+++ SehirRehberi.API/Controllers/CitiesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SehirRehberi.API.Repositories;
+using SehirRehberi.API.ViewModels;
+
+namespace SehirRehberi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly IAppRepository _appRepository;
+
+        public CitiesController(IAppRepository appRepository)
+        {
+            _appRepository = appRepository;
+        }
+
+        public ActionResult GetCities()
+        {
+            var cities = _appRepository.GetCities().Select(i => new CityViewModel { Description = i.Description, Id = i.Id, Name = i.Name, PhotoUrl = i.Photos.FirstOrDefault(a => a.IsMain == true).Url }).ToList();
+            return Ok(cities);
+        }
+    }
+}
---
+++ SehirRehberi.API/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using SehirRehberi.API.Models;
+using SehirRehberi.API.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Helpers
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<City, CityViewModel>().ForMember(dest => dest.PhotoUrl, opt =>
+               {
+                   opt.MapFrom(src => src.Photos.FirstOrDefault(i => i.IsMain == true).Url);
+               });
+        }
+    }
+}
