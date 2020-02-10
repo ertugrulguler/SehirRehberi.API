c18f0a44fde83c8ef7803fd97da784c227788020
Ertuğrul Güler <ertglr.61@gmail.com>
2020-02-10 18:17:07 +0300

    Auth cotrollerda yanlış tanımlanan kısım düzenlendi.

--- SehirRehberi.API/Controllers/AuthController.cs
+++ SehirRehberi.API/Controllers/AuthController.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using SehirRehberi.API.Models;
 using SehirRehberi.API.Repositories.Entities;
+using SehirRehberi.API.Repositories.Interfaces;
 using SehirRehberi.API.ViewModels;
 
 namespace SehirRehberi.API.Controllers
@@ -15,9 +20,9 @@ namespace SehirRehberi.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        private AuthRepository _authRepository;
+        private IAuthRepository _authRepository;
         private IConfiguration _configuration;
-        public AuthController(AuthRepository authRepository, IConfiguration configuration)
+        public AuthController(IAuthRepository authRepository, IConfiguration configuration)
         {
             _authRepository = authRepository;
             _configuration = configuration;
@@ -43,6 +48,34 @@ namespace SehirRehberi.API.Controllers
 
             var createdUser = _authRepository.Register(userToCreate, userForRegister.Password);
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginViewModel userForLogin)
+        {
+            var user = await _authRepository.Login(userForLogin.UserName, userForLogin.Password);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Secret").Value);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                 {
+                     new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                     new Claim(ClaimTypes.Name, user.Username)
+                 }),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(tokenString);
+            
         }
     }
 }
